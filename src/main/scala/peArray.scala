d10faa18e7e15d0d9ed9ef2f414f5a717fab7a77
luoconghui <luoch28@mail2.sysu.edu.cn>
2021-09-16 21:20:37 +0800

    sync the code

--- src/main/scala/peArray.scala
+++ src/main/scala/peArray.scala
@@ -10,7 +10,29 @@ case class peArray(Ih : Int, Wh : Int, Ww : Int, width : Int) extends Component{
     val outMtx = out Vec(Vec(SInt(width bits), Ww), Ih)
   }
 
-  val peMtx = List.tabulate(Ih, Ww)((i,j) =>  peUnit(Wh, width))
+  val peMtx = List.tabulate(Ih, Ww)((i,j) => new peUnit(Wh, width))
+  for(i <- 0 until(Ih); j <- 0 until(Ww)){
+    peMtx(i)(j).io.fm_data := io.fmMat(i)
+    peMtx(i)(j).io.weight := io.wgtMat(j)
+    io.outMtx(i)(j) := peMtx(i)(j).io.result
+  }
+
+}
+
+case class peArrayCtrl(Ih : Int, Wh : Int, Ww : Int, width : Int, Ni : Int, Hout : Int) extends Component{
+  val weightType = HardType(Vec(Vec(SInt(width bits), Wh), Ww))
+  val fmInType = HardType(Vec(Vec(SInt(width bits), Wh), Ih))
+  val fmOutType = HardType(Vec(Vec(SInt(width bits), Ww), Ih))
+
+  val io = new Bundle{
+    val weightIn = slave Stream(weightType)
+    val fmIn = slave Stream(fmInType)
+    val fmOut = master Stream(fmOutType)
+    val peSwitch = out Bool()
+  }
+   val
+
+
 }
 
 object peArray {
